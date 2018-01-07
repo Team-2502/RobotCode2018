eb6f27ead8064dbea73334f0aa35570a5dea4f9e
Ritik Mishra <ritik.mishra314@gmail.com>
2018-01-07 00:14:32 -0600

    Can now get game message

--- src/com/team2502/robot2018/Robot.java
+++ src/com/team2502/robot2018/Robot.java
@@ -7,12 +7,14 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.DriverStation;
 import logger.Log;
 
 public final class Robot extends IterativeRobot
 {
     public static DriveTrainSubsystem DRIVE_TRAIN;
     public static ClimberSubsystem CLIMBER;
+    public static String GAME_DATA; //TODO: Have better name
 
     // NavX Subsystem
     public static AHRS NAVX;
@@ -49,6 +51,7 @@ public final class Robot extends IterativeRobot
     {
         Scheduler.getInstance().run();
         DashboardData.update();
+        GAME_DATA = DriverStation.getInstance().getGameSpecificMessage();
     }
 
     /**
