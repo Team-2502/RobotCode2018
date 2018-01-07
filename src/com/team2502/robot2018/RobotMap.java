c95ec137284cc22346cf01d75df81047c47240db
Ritik Mishra <ritik.mishra314@gmail.com>
2018-01-06 20:22:35 -0600

    good ids

--- src/com/team2502/robot2018/RobotMap.java
+++ src/com/team2502/robot2018/RobotMap.java
@@ -16,6 +16,8 @@ public class RobotMap
 
         public static final class Button
         {
+            public static final int FORCE_LOW_GEAR = UNDEFINED;
+
             private Button() {}
         }
     }
@@ -27,15 +29,24 @@ public class RobotMap
 
     public static final class Motor
     {
+        public static final double SHIFT_UP_THRESHOLD = UNDEFINED;
+        public static final double SHIFT_DOWN_THRESHOLD = UNDEFINED;
+
         private Motor() {}
 
         // TODO: Set proper values.
-        public static final int DRIVE_TRAIN_FRONT_LEFT = UNDEFINED;
-        public static final int DRIVE_TRAIN_BACK_LEFT = UNDEFINED;
-        public static final int DRIVE_TRAIN_FRONT_RIGHT = UNDEFINED;
-        public static final int DRIVE_TRAIN_BACK_RIGHT = UNDEFINED;
+        public static final int DRIVE_TRAIN_FRONT_LEFT = 12;
+        public static final int DRIVE_TRAIN_BACK_LEFT_ENC = 4;
+        public static final int DRIVE_TRAIN_FRONT_RIGHT = 8;
+        public static final int DRIVE_TRAIN_BACK_RIGHT_ENC = 7;
 
         public static final int CLIMBER_LEFT = UNDEFINED;
         public static final int CLIMBER_RIGHT = UNDEFINED;
+    }
+
+    public class Solenoid {
+        private Solenoid() {}
+
+        public static final int TRANSMISSION_SWITCH = UNDEFINED;
     }
 }
