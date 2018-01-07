ff7475f8c20fb01c232f6fa712ea89d5418582dd
donovanMaas <donovan.Maas@team2502.com>
2018-01-07 14:34:27 -0600

    Improved `DriveTime` comment.
    Made comment act more like documentation.

--- src/com/team2502/robot2018/command/autonomous/ingredients/DriveTime.java
+++ src/com/team2502/robot2018/command/autonomous/ingredients/DriveTime.java
@@ -1,5 +1,9 @@
 package com.team2502.robot2018.command.autonomous.ingredients;
 
+/**
+ * Isaac said this would be a good idea for a filler
+ */
 public class DriveTime
-{ //Isaac said this would be a good idea for a filler
+{
+    
 }
