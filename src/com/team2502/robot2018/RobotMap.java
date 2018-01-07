1ac62c0ef305a35bd87f0236838d475baf3e8b0f
donovanMaas <donovan.Maas@team2502.com>
2018-01-06 20:09:01 -0600

    Added annotations to handle undefined variables.
    Due to problems with `RobotMap` id's being `-1` in production code I have created an annotation which when placed on a variable will either cause a compile time error or warning depending on whether the `safe` variable is set for the annotation.

--- src/com/team2502/robot2018/RobotMap.java
+++ src/com/team2502/robot2018/RobotMap.java
@@ -1,5 +1,8 @@
 package com.team2502.robot2018;
 
+
+import com.team2502.undefinedprocessor.Undefined;
+
 public class RobotMap
 {
     /**
@@ -35,12 +38,18 @@ public class RobotMap
         private Motor() {}
 
         // TODO: Set proper values.
+        @Undefined
         public static final int DRIVE_TRAIN_FRONT_LEFT = UNDEFINED;
+        @Undefined
         public static final int DRIVE_TRAIN_BACK_LEFT = UNDEFINED;
+        @Undefined
         public static final int DRIVE_TRAIN_FRONT_RIGHT = UNDEFINED;
+        @Undefined
         public static final int DRIVE_TRAIN_BACK_RIGHT = UNDEFINED;
 
+        @Undefined(safe = true)
         public static final int CLIMBER_LEFT = UNDEFINED;
+        @Undefined(safe = true)
         public static final int CLIMBER_RIGHT = UNDEFINED;
     }
 }
