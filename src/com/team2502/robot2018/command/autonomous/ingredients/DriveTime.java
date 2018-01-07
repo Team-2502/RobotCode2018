85003050a033e661bb57a73cb3f189c00d69246a
Isaac Ash-Johnson <isaacashj@gmail.com>
2018-01-07 15:17:55 -0600

    Turned DriveTime into a real command

--- src/com/team2502/robot2018/command/autonomous/ingredients/DriveTime.java
+++ src/com/team2502/robot2018/command/autonomous/ingredients/DriveTime.java
@@ -1,9 +1,16 @@
 package com.team2502.robot2018.command.autonomous.ingredients;
 
+import edu.wpi.first.wpilibj.command.Command;
+
 /**
  * Isaac said this would be a good idea for a filler
  */
-public class DriveTime
+public class DriveTime extends Command
 {
 
+    @Override
+    protected boolean isFinished()
+    {
+        return false;
+    }
 }
