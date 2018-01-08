1395b5d279454950042adfca9f324fc0c1ea36c1
Ritik Mishra <ritik.mishra314@gmail.com>
2018-01-07 23:57:58 -0600

    Interface for subsystems that use SmartDashboard

--- src/com/team2502/robot2018/AutoSwitcher.java
+++ src/com/team2502/robot2018/AutoSwitcher.java
@@ -6,45 +6,58 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 class AutoSwitcher
 {
-    private static SendableChooser<AutoMode> autoChooser;
+	private static SendableChooser<AutoMode> autoChooser;
 
-    public enum AutoMode
-    {
-        // TODO: Remove this
-        DEMO("Demo", null);
+	public enum AutoMode
+	{
+		// TODO: Remove this
+		DEMO("Demo", null);
 
 
-        public final Class<? extends Command> autoCommand;
-        public final String name;
+		public final Class<? extends Command> autoCommand;
+		public final String name;
 
-        AutoMode(String name, Class<? extends Command> autoCommand)
-        {
-            this.autoCommand = autoCommand;
-            this.name = name;
-        }
+		AutoMode(String name, Class<? extends Command> autoCommand)
+		{
+			this.autoCommand = autoCommand;
+			this.name = name;
+		}
 
-        public Command getInstance()
-        {
-            Command instance;
-            try { instance = autoCommand.newInstance(); }
-            catch(InstantiationException | IllegalAccessException e) { return null; }
-            return instance;
-        }
-    }
+		public Command getInstance()
+		{
+			Command instance;
+			try
+			{
+				instance = autoCommand.newInstance();
+			} catch (InstantiationException | IllegalAccessException e)
+			{
+				return null;
+			}
+			return instance;
+		}
+	}
 
-    static void putToSmartDashboard()
-    {
-        autoChooser = new SendableChooser<AutoMode>();
+	static void putToSmartDashboard()
+	{
+		autoChooser = new SendableChooser<AutoMode>();
 
-        for(int i = 0; i < AutoMode.values().length; i++)
-        {
-            AutoMode mode = AutoMode.values()[i];
-            if(i == 0) { autoChooser.addDefault(mode.name, mode); }
-            else { autoChooser.addObject(mode.name, mode); }
-        }
+		for (int i = 0; i < AutoMode.values().length; i++)
+		{
+			AutoMode mode = AutoMode.values()[i];
+			if (i == 0)
+			{
+				autoChooser.addDefault(mode.name, mode);
+			} else
+			{
+				autoChooser.addObject(mode.name, mode);
+			}
+		}
 
-        SmartDashboard.putData("auto_modes", autoChooser);
-    }
+		SmartDashboard.putData("auto_modes", autoChooser);
+	}
 
-    static Command getAutoInstance() { return autoChooser.getSelected().getInstance(); }
+	static Command getAutoInstance()
+	{
+		return autoChooser.getSelected().getInstance();
+	}
 }
--- src/com/team2502/robot2018/OI.java
+++ src/com/team2502/robot2018/OI.java
@@ -4,21 +4,30 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public final class OI
 {
-    public static final Joystick JOYSTICK_DRIVE_LEFT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_LEFT);
-    public static final Joystick JOYSTICK_DRIVE_RIGHT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_RIGHT);
-    public static final Joystick JOYSTICK_FUNCTION = new Joystick(RobotMap.Joystick.JOYSTICK_FUNCTION);
+	public static final Joystick JOYSTICK_DRIVE_LEFT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_LEFT);
+	public static final Joystick JOYSTICK_DRIVE_RIGHT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_RIGHT);
+	public static final Joystick JOYSTICK_FUNCTION = new Joystick(RobotMap.Joystick.JOYSTICK_FUNCTION);
 
-    static
-    {
-    }
+	static
+	{
+	}
 
-    public static void init() {}
+	public static void init()
+	{
+	}
 
-    public static boolean joysThreshold(double threshold, boolean above)
-    {
-        if(above) { return Math.abs(OI.JOYSTICK_DRIVE_RIGHT.getY()) > threshold && Math.abs(OI.JOYSTICK_DRIVE_LEFT.getY()) > threshold; }
-        else { return Math.abs(OI.JOYSTICK_DRIVE_RIGHT.getY()) < threshold && Math.abs(OI.JOYSTICK_DRIVE_LEFT.getY()) < threshold; }
-    }
+	public static boolean joysThreshold(double threshold, boolean above)
+	{
+		if (above)
+		{
+			return Math.abs(OI.JOYSTICK_DRIVE_RIGHT.getY()) > threshold && Math.abs(OI.JOYSTICK_DRIVE_LEFT.getY()) > threshold;
+		} else
+		{
+			return Math.abs(OI.JOYSTICK_DRIVE_RIGHT.getY()) < threshold && Math.abs(OI.JOYSTICK_DRIVE_LEFT.getY()) < threshold;
+		}
+	}
 
-    private OI() {}
+	private OI()
+	{
+	}
 }
