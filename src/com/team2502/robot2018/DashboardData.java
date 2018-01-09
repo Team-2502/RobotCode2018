ed44a7fb0d4f87b9526756315e402c2717e735e6
Donovan <donovanMaas@users.noreply.github.com>
2018-01-09 14:47:32 -0600

    Added documentation explaining design decisions. (#17)
    Added documentation showing why an ArrayList is better than a LinkedList for storing the number `DashboardUpdaters`.

--- src/com/team2502/robot2018/DashboardData.java
+++ src/com/team2502/robot2018/DashboardData.java
@@ -20,12 +20,42 @@ public final class DashboardData
     }
 
     /**
-     * 64 + 4 + 16 bytes
-     * 64 = 16 * 4 (4 => the size of a pointer)
-     * 4 => The size of the internal `size` variable
-     * 16 => The JVM internal data for an array
+     * ArrayList:
+     * 12 + 16 + 4 + 4n
+     *   12 bytes for the object header
+     *     4 bytes for the class pointer (type)
+     *     4 bytes for the flags
+     *     4 bytes for the thread locking data
+     *   16 bytes for the array header
+     *     4 bytes for the class pointer (type)
+     *     4 bytes for the flags
+     *     4 bytes for the thread locking data
+     *     4 bytes for the length of the array
+     *   4 bytes for the length of the list
+     *   4n bytes where `n` is the size of the array, 4 is the size of a pointer
+     *
+     * Doubly Linked List:
+     * (12 + 4 + 4 + 4)n
+     *   12 bytes for the object header
+     *     4 bytes for the class pointer (type)
+     *     4 bytes for the flags
+     *     4 bytes for the thread locking data
+     *   4 bytes for the pointer to the previous element
+     *   4 bytes for the pointer to the next element
+     *   4 bytes for the pointer to the actual data.
+     *   n is the number of elements in the list
+     *
+     * Under 2 elements the LinkedList will be smaller in size.
+     * Anything larger the ArrayList will be smaller (to an extent,
+     * the ArrayList expands at a rate of (n * 1.5) so the next time
+     * allocates it may be larger)
+     *
+     * We are allocating 4 elements as a starter. I would recommend
+     * setting this to `2^(floor(log2(numberOfallocators))`. This will
+     * Keep the processor happy with padding, and still allows it to
+     * dynamically expand without much delay at launch or high memory usage.
      */
-    private static List<DashboardUpdater> updaters = new ArrayList<DashboardUpdater>(16);
+    private static List<DashboardUpdater> updaters = new ArrayList<DashboardUpdater>(4);
 
     static void update()
     {
