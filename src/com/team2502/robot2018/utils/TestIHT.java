3705dd63d544fa6096fd11776343c8ec5b9d4a77
Ritik Mishra <ritik.mishra314@gmail.com>
2018-01-21 23:54:02 -0600

    interpolating hash table utilising linear regression; useful for adaptive pure pursuit

---
+++ src/com/team2502/robot2018/utils/TestIHT.java
@@ -0,0 +1,28 @@
+package com.team2502.robot2018.utils;
+
+/**
+ * Not meant to be used in robot code, just on our computrons.
+ */
+class TestIHT
+{
+    public static void main(String[] args)
+    {
+        InterpolatingHashTable wheee = new InterpolatingHashTable(0, 1);
+        System.out.println(wheee.get(100)); // should be 1
+        System.out.println(wheee.get(-100)); // should be 1
+
+        wheee.put(100, 101); // slope: 1
+
+        System.out.println(wheee.get(60)); // should be 61
+        System.out.println(wheee.get(-60)); // should be 1
+        System.out.println(wheee.get(600)); // should be 101
+
+        wheee.put(200, 1); // slope: -1
+
+
+        System.out.println(wheee.get(60)); // should be 61
+        System.out.println(wheee.get(100)); // should be 101
+        System.out.println(wheee.get(150)); // should be 51
+
+    }
+}
