7ad1deff8b9d903f85c667aa138c4c027f09e35f
MDW01 <michaeldwagner@me.com>
2018-02-03 20:21:20 -0600

    Updated Talon IDS for 2018

--- src/com/team2502/robot2018/RobotMap.java
+++ src/com/team2502/robot2018/RobotMap.java
@@ -45,11 +45,10 @@ public class RobotMap
 
     public static final class Motor
     {
-        // TODO: Set proper values.
-        public static final int DRIVE_TRAIN_FRONT_LEFT = 8;
-        public static final int DRIVE_TRAIN_BACK_LEFT = 7;
-        public static final int DRIVE_TRAIN_FRONT_RIGHT = 4;
-        public static final int DRIVE_TRAIN_BACK_RIGHT = 12;
+        public static final int DRIVE_TRAIN_FRONT_RIGHT = 1;
+        public static final int DRIVE_TRAIN_BACK_RIGHT = 2;
+        public static final int DRIVE_TRAIN_FRONT_LEFT = 3;
+        public static final int DRIVE_TRAIN_BACK_LEFT = 4;
 
         @Undefined(safe = true)
         public static final int CLIMBER_LEFT = UNDEFINED;
