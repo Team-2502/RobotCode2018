6278818990a952e4fe5e60a453f648e585b70441
MDW01 <michaeldwagner@me.com>
2018-02-12 10:32:29 -0600

    Fixed Diagram

--- src/com/team2502/robot2018/RobotMap.java
+++ src/com/team2502/robot2018/RobotMap.java
@@ -71,11 +71,11 @@ public class RobotMap
          *          Left Side of PDP         |           Right Side of PDP
          *                                   |
          * Spare Talon ------------- 7 - 11  |  Active Rotate ----------- 6 -- 6
-         * Active Left ------------- 8 - 12  |  Active_Right ------------ 5 -- 5
-         * Elevator Top ------------ 9 - 13  |  Drive_Train_Back_Left --- 4 -- 4
-         * Elevator Bottom -------- 10 - 14  |  Drive_Train_Front_Left -- 3 -- 3
-         * Climber Top ------------ 11 - 15  |  Drive_Train_Back_Right -- 2 -- 2
-         * Climber Bottom --------- 12 - 16  |  Drive_Train_Front_Right - 1 -- 1
+         * Active Left ------------- 8 - 12  |  Active Right ------------ 5 -- 5
+         * Elevator Top ------------ 9 - 13  |  Drive Train Back Left --- 4 -- 4
+         * Elevator Bottom -------- 10 - 14  |  Drive Train Front Left -- 3 -- 3
+         * Climber Top ------------ 11 - 15  |  Drive Train Back Right -- 2 -- 2
+         * Climber Bottom --------- 12 - 16  |  Drive Train Front Right - 1 -- 1
          */
 
         public static final int DRIVE_TRAIN_FRONT_RIGHT = 1;
