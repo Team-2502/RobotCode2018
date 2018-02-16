3edc3a59db219a66402772c4c943827c6f992616
Isaac Ash-Johnson <isaacashj@gmail.com>
2018-02-16 09:17:27 -0600

    No more need for debounced joysticks
    We no longer do that sketchy stuff in the Elevator periodic method, as the method doesn't quit itself randomly anymore.

--- src/com/team2502/robot2018/OI.java
+++ src/com/team2502/robot2018/OI.java
@@ -1,45 +1,46 @@
 package com.team2502.robot2018;
 
 import com.team2502.robot2018.command.teleop.*;
-import com.team2502.robot2018.utils.baseoverloads.JoystickButtonF;
-import com.team2502.robot2018.utils.baseoverloads.JoystickF;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * The Operator Interface class
  */
 public final class OI
 {
-    public static final JoystickF JOYSTICK_DRIVE_LEFT = new JoystickF(RobotMap.Joystick.JOYSTICK_DRIVE_LEFT);
-    public static final JoystickF JOYSTICK_DRIVE_RIGHT = new JoystickF(RobotMap.Joystick.JOYSTICK_DRIVE_RIGHT);
-    public static final JoystickF JOYSTICK_FUNCTION = new JoystickF(RobotMap.Joystick.JOYSTICK_FUNCTION);
+    public static final Joystick JOYSTICK_DRIVE_LEFT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_LEFT);
+    public static final Joystick JOYSTICK_DRIVE_RIGHT = new Joystick(RobotMap.Joystick.JOYSTICK_DRIVE_RIGHT);
+    public static final Joystick JOYSTICK_FUNCTION = new Joystick(RobotMap.Joystick.JOYSTICK_FUNCTION);
 
-    public static final Button INTAKE_IN = new DebouncedJoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.INTAKE_IN);
-    public static final Button INTAKE_OUT = new DebouncedJoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.INTAKE_OUT);
-    public static final Button OPEN_INTAKE = new DebouncedJoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.OPEN_INTAKE);
+    public static final Button INTAKE_IN = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.INTAKE_IN);
+    public static final Button INTAKE_OUT = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.INTAKE_OUT);
+    public static final Button OPEN_INTAKE = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.OPEN_INTAKE);
 
-    public static final Button ELEV_UP = new DebouncedJoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.RAISE_ELEVATOR);
-    public static final Button ELEV_DOWN = new DebouncedJoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.LOWER_ELEVATOR);
+    public static final Button ELEV_UP = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.RAISE_ELEVATOR);
+    public static final Button ELEV_DOWN = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.LOWER_ELEVATOR);
 
-//    public static final Button CLIMBER = new JoystickButtonF(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.CLIMBER);
-//    public static final Button CLIMBER_DOWN = new JoystickButtonF(JOYSTICK_FUNCTION, RobotMap.UNDEFINED);
+    public static final Button CLIMBER = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.CLIMBER);
 
-
-    public static final Button SHIFT_GEARBOX_ELEV = new DebouncedJoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.SHIFT_GEARBOX_ELEV);
+    public static final Button SHIFT_GEARBOX_ELEV = new JoystickButton(JOYSTICK_FUNCTION, RobotMap.Joystick.Button.SHIFT_GEARBOX_ELEV);
 
     private OI() { }
 
     public static void init()
     {
+        // Elevator buttons
+        ELEV_UP.whileHeld(new ElevatorCommand(1.0));
+        ELEV_DOWN.whileHeld(new ElevatorCommand( -0.4));
+
         // Active Intake buttons
         INTAKE_IN.whileHeld(new ActiveIntakeCommand(0.6));
         INTAKE_OUT.whileHeld(new ActiveIntakeCommand(-0.6));
 
         OPEN_INTAKE.whenPressed(new GrabCommand());
 
+        // Climber button (wait to re-implement until elevator is working properly
 //        CLIMBER.whileHeld(new ClimberCommand(1));
-//        CLIMBER_DOWN.whileHeld(new ClimberCommand(-1));
-
 
         SHIFT_GEARBOX_ELEV.whenPressed(new ShiftElevatorCommand());
     }
