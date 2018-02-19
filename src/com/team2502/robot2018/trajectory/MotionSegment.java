505e4a0177e939a1034fcbafa5a679948d75fed2
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-02-19 13:55:42 -0600

    make code compilable

--- src/com/team2502/robot2018/trajectory/MotionSegment.java
+++ src/com/team2502/robot2018/trajectory/MotionSegment.java
@@ -5,6 +5,7 @@ import com.team2502.robot2018.utils.MathUtils;
 
 /**
  * A linear segment joining two {@link MotionState1D}s.
+ * @deprecated Doesn't really need to be used anywhere... and not working (probably) anyway
  */
 public class MotionSegment
 {
@@ -36,7 +37,8 @@ public class MotionSegment
 
     public boolean valid()
     {
-        return MathUtils.epsilonEquals(start.extrapForPositionAtTime(end.getTime()), end.getPosition());
+        return false;
+//        return MathUtils.epsilonEquals(start.extrapForPositionAtTime(end.getTime()), end.getPosition());
     }
 
     public boolean containsPosition(float position)
