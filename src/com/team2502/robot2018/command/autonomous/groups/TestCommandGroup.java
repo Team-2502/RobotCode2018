4121c16c2b5fef77a3d8847a7fd27cc8325511c3
Ritik Mishra <ritik.mishra314@gmail.com>
2018-03-05 11:02:33 -0600

    Added a test for it

--- src/com/team2502/robot2018/command/autonomous/groups/TestCommandGroup.java
+++ src/com/team2502/robot2018/command/autonomous/groups/TestCommandGroup.java
@@ -1,5 +1,7 @@
 package com.team2502.robot2018.command.autonomous.groups;
 
+import com.team2502.robot2018.Constants;
+import com.team2502.robot2018.command.autonomous.ingredients.ElevatorAutonCommand;
 import com.team2502.robot2018.command.autonomous.ingredients.Paths;
 import com.team2502.robot2018.command.autonomous.ingredients.PurePursuitCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -8,6 +10,9 @@ public class TestCommandGroup extends CommandGroup
 {
     public TestCommandGroup()
     {
-        addSequential(new PurePursuitCommand(Paths.Center.rightSwitch));
+        addSequential(new ElevatorAutonCommand(3, Constants.SWITCH_ELEV_HEIGHT_FT));
+        addSequential(new ElevatorAutonCommand(3, 0));
+        addSequential(new ElevatorAutonCommand(3, Constants.SCALE_ELEV_HEIGHT_FT));
+        addSequential(new ElevatorAutonCommand(3, 0));
     }
 }
