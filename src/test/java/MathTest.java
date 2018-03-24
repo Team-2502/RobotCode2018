9bf5d6c0c35e16db9e04ac1e5529838a79ea5d08
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-03-24 15:53:09 -0500

    add basic quick maths unit test

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MathTest
+{
+    @Test
+    public void testQuickMaths()
+    {
+        assertEquals(2+2,4);
+        assertEquals(4-1,3);
+    }
+}
