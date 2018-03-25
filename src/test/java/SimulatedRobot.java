af2637f556a26ee378ef7ea3e39eb34c231024bd
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-03-25 11:11:57 -0500

    add start of a simulated robot for unit testing

---
+++ src/test/java/SimulatedRobot.java
@@ -0,0 +1,23 @@
+import org.joml.ImmutableVector2f;
+
+public class SimulatedRobot
+{
+
+    public static final float MAX_VEL = 16F;
+
+    ImmutableVector2f position;
+    float heading;
+
+    float leftMotorPercentVoltage;
+    float rightMotorPercentVoltage;
+
+    public void runMotorsVelocity()
+    {
+
+    }
+
+    public void runMotorsPosition()
+    {
+
+    }
+}
