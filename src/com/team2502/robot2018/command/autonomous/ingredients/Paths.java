0c984d9d566e27d045a8c681a280f8889a304cd3
Ritik Mishra <ritik.mishra314@gmail.com>
2018-03-30 23:30:19 -0500

    (UNTESTED) Change same side scale auton to go to the left side of the scale
    This will make our robot compatible with 2052's cross country scale autonomous because they come up on the near side of the scale, so we must be out of their way.
    It is not guaranteed that we will be with 2052 in elims, however it is very likely that an alliance partner will have a cross country scale auto
    These points came from the CAD model of the field and should be very close if not perfect.

--- src/com/team2502/robot2018/command/autonomous/ingredients/Paths.java
+++ src/com/team2502/robot2018/command/autonomous/ingredients/Paths.java
@@ -63,9 +63,10 @@ public class Paths
         public static final List<Waypoint> leftScale = Arrays.asList(
                 new Waypoint(new ImmutableVector2f(0, 0), 0, 20, -9),
                 new Waypoint(new ImmutableVector2f(-0.5F, 8.589F), 16F, 20, -7, true),
-                new Waypoint(new ImmutableVector2f(-0.5F, 12.405F), 5F, 20, -7, true, new RaiseElevatorScale()),
-                new Waypoint(new ImmutableVector2f(-0.5F, 20.5F), 3F, 20, -7, true),
-                new Waypoint(new ImmutableVector2f(2.3F, 23.5F), 0F, 20, -7) // max deceleration appears to be -7 ft / s^2
+                new Waypoint(new ImmutableVector2f(-0.5F, 12.405F), 15F, 20, -7, true, new RaiseElevatorScale()),
+                new Waypoint(new ImmutableVector2f(-0.5F, 20.5F), 5F, 20, -7, true),
+                new Waypoint(new ImmutableVector2f(-1.5F, 25F), 3F, 20, -7),
+                new Waypoint(new ImmutableVector2f(1.5F, 25.375F), 0F, 20, -7)
                                                                     );
 
         public static final List<Waypoint> leftScaleToSwitch = Arrays.asList(
